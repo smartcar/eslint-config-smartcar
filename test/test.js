b898e4da74c01e17a7722711867ea9601261ec85
Gurpreet Atwal <gurpreetatwal@users.noreply.github.com>
2016-06-27 17:06:56 -0700

    setup some basic tests

---
+++ test/test.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var test = require('ava');
+var eslint = require('eslint');
+var tempWrite = require('temp-write');
+var isPlainObj = require('is-plain-obj');
+
+function runEslint(str, conf) {
+  var linter = new eslint.CLIEngine({
+    useEslintrc: false,
+    configFile: tempWrite.sync(JSON.stringify(conf))
+  });
+
+  return linter.executeOnText(str).results[0].messages;
+}
+
+test('main', function(t) {
+  var conf = require('../');
+
+  t.true(isPlainObj(conf));
+  t.true(isPlainObj(conf.rules));
+
+  var errors = runEslint('\'use strict\';\nconsole.log("unicorn")\n', conf);
+  t.is(errors[0].ruleId, 'quotes');
+});
+
+test('browser',function(t) {
+  var conf = require('../browser');
+
+  t.true(isPlainObj(conf));
+
+  var errors = runEslint('\'use strict\';\nprocess.exit();\n', conf);
+  t.is(errors[0].ruleId, 'no-undef');
+});
