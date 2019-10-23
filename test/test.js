76565a2f1f2b42769f2f866c895bba5a3b7c7e22
Gurpreet Atwal <git@gatwal.com>
2019-10-22 21:37:07 -0700

    feat: turn off all rules that conflict with prettier
    closes #42
    
    BREAKING CHANGE: All rules that conflict with `prettier` will no longer
    be checked.

--- test/test.js
+++ test/test.js
@@ -1,25 +1,42 @@
 'use strict';
 
-const test = require('ava');
 const eslint = require('eslint');
-const tempWrite = require('temp-write');
+const eslintConfigPrettier = require('eslint-config-prettier');
 const isPlainObj = require('is-plain-obj');
+const test = require('ava');
+const {join} = require('path');
 
 const config = require('../');
 
-function runEslint(str, conf) {
-  const linter = new eslint.CLIEngine({
-    useEslintrc: false,
-    configFile: tempWrite.sync(JSON.stringify(conf)),
-  });
-
-  return linter.executeOnText(str).results[0].messages;
-}
-
-test('main', function(t) {
+test('type check', (t) => {
   t.true(isPlainObj(config));
   t.true(isPlainObj(config.rules));
+});
 
-  const errors = runEslint('\'use strict\';\nconsole.log("unicorn")\n', config);
+test('integration', (t) => {
+  const linter = new eslint.CLIEngine({
+    configFile: join(process.cwd(), 'index.js'),
+  });
+
+  const code = '\'use strict\';\nconsole.log("unicorn")\n';
+  const errors = linter.executeOnText(code).results[0].messages;
+
   t.is(errors[0].ruleId, 'no-console');
+});
+
+test('prettier rules', (t) => {
+  const rules = Object.entries(config.rules);
+
+  rules.forEach(([key, value]) => {
+    // prettier rules are turned off by using 0 as opposed to
+    // "off" for this test case
+    if (value !== 0) {
+      return;
+    }
+
+    t.true(
+      key in eslintConfigPrettier.rules,
+      `${key} should not be marked as off because of prettier`
+    );
+  });
 });
