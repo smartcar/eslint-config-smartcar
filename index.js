bdcba74dcb6724d5ab253a5b6209fe9bf2bd4973
Gurpreet Atwal <git@gatwal.com>
2019-12-23 15:03:02 -0800

    feat: setup `array-func` plugin and all rules

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ module.exports = {
     },
   ],
   plugins: [
+    'array-func',
     'eslint-comments',
     'import',
     'no-use-extend-native',
@@ -342,6 +343,18 @@ module.exports = {
     //
     // PLUGINS
     //
+
+    /**
+     * eslint-plugin-array-func
+     *
+     * @see https://github.com/freaktechnik/eslint-plugin-array-func
+     */
+    'array-func/from-map': 'error',
+    'array-func/no-unnecessary-this-arg': 'error',
+    'array-func/prefer-array-from': 'error',
+    'array-func/avoid-reverse': 'error',
+    'array-func/prefer-flat-map': 'error',
+    'array-func/prefer-flat': 'error',
 
     /**
      * eslint-plugin-comments
