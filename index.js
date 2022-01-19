a4dcc505c0e2dc00083f90381598b3ff762872b7
Gurpreet Atwal <git@gatwal.com>
2022-01-18 23:43:40 -0800

    feat: bump eslint-plugin-sonarjs from 0.6.0 to 0.13.0
    - 0.6.0 -> 0.8.0 - did not have any big changes, just small improvements to two rules
      https://github.com/SonarSource/eslint-plugin-sonarjs/compare/v0.6.0...0.8.0-125
    - 0.9.x - introduces the following new rules:
       - no-empty-collection
       - no-ignored-return
       - non-existent-operator
       - elseif-without-else
       - no-gratuitous-expressions
       - no-nested-switch
       - no-nested-template-literals
    - 0.10.0 - small improvements
    - 0.11.0 - small improvements + ESLint 8.x support
    - 0.12.0 -
       - no-identical-functions - added min function size option
    - 0.13.0 - suggestions added for 5 rules
    
    https://github.com/SonarSource/eslint-plugin-sonarjs/releases

--- index.js
+++ index.js
@@ -655,21 +655,29 @@ module.exports = {
     // Bug Detection
     'sonarjs/no-all-duplicated-branches': 'error',
     'sonarjs/no-element-overwrite': 'error',
+    'sonarjs/no-empty-collection': 'error',
     'sonarjs/no-extra-arguments': 'error',
     'sonarjs/no-identical-conditions': 'off', // use `no-dupe-else-if`
     'sonarjs/no-identical-expressions': 'error',
+    'sonarjs/no-ignored-return': 'error',
     'sonarjs/no-one-iteration-loop': 'error',
     'sonarjs/no-use-of-empty-return-value': 'error',
+    'sonarjs/non-existent-operator': 'error',
 
     // Code Smell Detection
     'sonarjs/cognitive-complexity': ['error', 15],
+    // too opinionated, prevents easy documentation of expected cases
+    'sonarjs/elseif-without-else': 'off',
     'sonarjs/max-switch-cases': ['error', 15],
     'sonarjs/no-collapsible-if': 'off', // too opinionated
     'sonarjs/no-collection-size-mischeck': 'error',
     'sonarjs/no-duplicate-string': ['error', 4],
     'sonarjs/no-duplicated-branches': 'error',
-    'sonarjs/no-identical-functions': 'error',
+    'sonarjs/no-gratuitous-expressions': 'error',
+    'sonarjs/no-identical-functions': ['error', 3],
     'sonarjs/no-inverted-boolean-check': 'error',
+    'sonarjs/no-nested-switch': 'error',
+    'sonarjs/no-nested-template-literals': 'error',
     'sonarjs/no-redundant-boolean': 'error',
     'sonarjs/no-redundant-jump': 'error',
     'sonarjs/no-same-line-conditional': 'off', // prettier
